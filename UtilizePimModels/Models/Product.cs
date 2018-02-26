08b9d60ac1470cd75241cd9bc796f7919c4d3d9b
Jeffrey Breedijk <j.c.breedijk@gmail.com>
2018-02-26 20:02:33 +0100

    Refactor

---
+++ UtilizePimModels/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace UtilizePim.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public bool ShowOnWebshop { get; set; }
+        public bool IsDeleted { get; set; }
+        public Dictionary<string, string> ShortDescription { get; set; }
+        public Dictionary<string, string> LongDescription { get; set; }
+        public Dictionary<string, double> Unit { get; set; }
+        public Dictionary<string, object> Properties { get; set; } 
+        public HashSet<string> Categories { get; set; }
+        public string ProductVariantKey { get; set; }
+        public HashSet<string> CustomerGrants { get; set; }
+        public HashSet<string> CustomerExclusions { get; set; }
+        public Dictionary<string, double> Price { get; set; }
+        
+    }
+}
